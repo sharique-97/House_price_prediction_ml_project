9080f3888736af41eca968ccb1ba1a37833e25ab
Sharique <ansarisharique.97@gmail.com>
2022-09-13 19:57:46 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from ensurepip import version
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 # Declaring variables for setup functions
@@ -19,14 +19,14 @@ def get_requirements_list()->List[str]:
 
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
 name = PROJECT_NAME,
 version= VERSION,
 author= AUTHOR,
 description= DESCRIPTION,
-packages= PACKAGES,
+packages= find_packages(),
 install_requires = get_requirements_list()
 
 

684ac658bb1d76bc39486bee49ddb44542c89e8b
Sharique <ansarisharique.97@gmail.com>
2022-12-16 01:27:22 +0530

    added config.yaml file

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -15,4 +15,3 @@ ModelTrainingConfig = namedtuple('ModelTrainingConfig',['trained_model_file_path
 ModelEvaluationConfig = namedtuple('ModelEvaluationConfig',['model_evaluation_file_path','time_stamp'])
 
 ModelPusherConfig = namedtuple('ModelPusherConfig',['export_dir_path'])
-

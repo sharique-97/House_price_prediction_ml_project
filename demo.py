b578d8683e3655131ecb0da5cfd322ae67d55fee
Sharique <ansarisharique.97@gmail.com>
2023-02-13 13:52:30 +0530

    updated EDA

--- demo.py
+++ demo.py
@@ -4,12 +4,12 @@ from housing.logger import logging
 from housing.config.configuration import Configuration
 def main():
     try:
-       pipeline = Pipeline()
-       pipeline.run_pipeline()
-       # data_validation_config = Configuration().get_data_validation_config()
-       # print(data_validation_config)
+      #  pipeline = Pipeline()
+      #  pipeline.run_pipeline()
+       data_validation_config = Configuration().get_data_transformation_config()
+       print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
 if __name__=='__main__':
-   main()
+   main()   

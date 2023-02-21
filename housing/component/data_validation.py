e0318a0998ecf0c7912a72ae268166b996b3836c
Sharique <ansarisharique.97@gmail.com>
2023-02-21 15:27:32 +0530

    Component Folder completed

--- housing/component/data_validation.py
+++ housing/component/data_validation.py
@@ -1,7 +1,6 @@
 from housing.logger import logging
 from housing.exception import HousingException
 from housing.entity.config_entity import DataValidationConfig
-from housing.entity.artifact_entity import DataIngestionArtifact
 from housing.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 import os,sys
 import pandas  as pd
@@ -12,11 +11,12 @@ from evidently.dashboard.tabs import DataDriftTab
 import json
 
 class DataValidation:
+    
 
     def __init__(self, data_validation_config:DataValidationConfig,
         data_ingestion_artifact:DataIngestionArtifact):
         try:
-            logging.info(f"{'='*20}Data Validation log started.{'='*20} \n\n")
+            logging.info(f"{'>>'*30}Data Valdaition log started.{'<<'*30} \n\n")
             self.data_validation_config = data_validation_config
             self.data_ingestion_artifact = data_ingestion_artifact
         except Exception as e:
@@ -37,11 +37,15 @@ class DataValidation:
             logging.info("Checking if training and test file is available")
             is_train_file_exist = False
             is_test_file_exist = False
+
             train_file_path = self.data_ingestion_artifact.train_file_path
             test_file_path = self.data_ingestion_artifact.test_file_path
+
             is_train_file_exist = os.path.exists(train_file_path)
             is_test_file_exist = os.path.exists(test_file_path)
+
             is_available =  is_train_file_exist and is_test_file_exist
+
             logging.info(f"Is train and test file exists?-> {is_available}")
             
             if not is_available:
@@ -50,11 +54,12 @@ class DataValidation:
                 message=f"Training file: {training_file} or Testing file: {testing_file}" \
                     "is not present"
                 raise Exception(message)
+
             return is_available
         except Exception as e:
             raise HousingException(e,sys) from e
 
-
+    
     def validate_dataset_schema(self)->bool:
         try:
             validation_status = False
@@ -68,6 +73,8 @@ class DataValidation:
             # NEAR BAY
             # NEAR OCEAN
             #3. Check column names
+
+
             validation_status = True
             return validation_status 
         except Exception as e:
@@ -75,8 +82,6 @@ class DataValidation:
 
     def get_and_save_data_drift_report(self):
         try:
-            self.is_train_test_file_exists()
-            self.validate_dataset_schema()
             profile = Profile(sections=[DataDriftProfileSection()])
 
             train_df,test_df = self.get_train_and_test_df()
@@ -134,6 +139,8 @@ class DataValidation:
             return data_validation_artifact
         except Exception as e:
             raise HousingException(e,sys) from e
-    
+
+
     def __del__(self):
-        logging.info(f"{'='*20}Data Validation log completed.{'='*20} \n\n")
+        logging.info(f"{'>>'*30}Data Valdaition log completed.{'<<'*30} \n\n")
+        
